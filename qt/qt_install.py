3c668876895f7271b22c57a968f7ac20bffbf64d
john_help <john_help@protonmail.com>
2020-04-13 22:52:33 +0800

    learn PyQt designer

---
+++ qt/qt_install.py
@@ -0,0 +1,23 @@
+PyQt5（designer）入门教程
+https://blog.csdn.net/azuremouse/article/details/90338961
+这一篇文章 比较合适，入门pyqt
+
+1, 安装 pip install PyQt5==5.13.0
+2. 安装 pip install pyqt5-tools==5.13.0
+3. 安装 pip install PyQtWebEngine=5.13.0
+
+检查 desiger安装 ：
+1. Win+S呼出Cornata主面板（搜索框），输入designer，可以启动 designer
+2. 命令行 输入 designer, 可以启动成功
+3. pyuic5 ，返回： “Error: one input ui-file must be specified” OK
+4. pyuic5 --version
+5. designer --version
+
+
+建立一个 qt_helloworld 项目
+跟着做，运行
+可以看到建立了一个窗口
+OK
+
+
+
